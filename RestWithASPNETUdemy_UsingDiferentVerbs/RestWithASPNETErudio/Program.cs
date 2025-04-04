abf4b57060c59cc86c68f4b2603ba0949bbe11c0
OtavioRamos56 <otavioramoadesoisa5@gmail>
2025-04-04 09:00:43 -0300

    Feat: Adding DiffetentVerbs

---
+++ RestWithASPNETUdemy_UsingDiferentVerbs/RestWithASPNETErudio/Program.cs
@@ -0,0 +1,24 @@
+using RestWithASPNETErudio.Services;
+using RestWithASPNETErudio.Services.Implementations;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+//Dependency Injection
+
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
